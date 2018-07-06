2ef116b67449ae8e2599da51e8c7f49a1807ed88
magda <magdalena.aniol@gmail.com>
2018-07-06 10:35:37 +0200

    add styles

--- components/Contact.js
+++ components/Contact.js
@@ -6,14 +6,12 @@ var Contact = React.createClass({
 	render: function() {
 		return (
 			React.createElement('div', {className: 'contactItem'},
-				React.createElement('img', {
-					className: 'contactImage',
-					src: 'images\\avatar.png'
-				}),
-				React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-				React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-				React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-					this.props.item.email
+				React.createElement('img', {className: 'contactImage',src: 'images\\icon.png'}),
+				React.createElement('div', {className: 'contactInfo'},
+					React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
+					React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
+					React.createElement('a', {className: 'contactEmail', href: this.props.item.email},
+					this.props.item.email)
 				)
 			)
 		)
