babe351e8ab265e0580b5087f7a106d7f9c279cd
magda <magdalena.aniol@gmail.com>
2018-07-06 14:19:02 +0200

    fix syntax error

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,10 +6,10 @@ var ContactForm = React.createClass({
 	render: function() {
 		return (
 			<form className={contactForm}>
-				<input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}>
-				<input type={'text'} placeholder={'Last Name'} value={this.props.contact.lastName}>
-				<input type={'email'} placeholder={'Email'} value={this.props.contact.email}>
-				<input type={'submit'} placeholder={'Add contact'}>
+				<input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}></input>
+				<input type={'text'} placeholder={'Last Name'} value={this.props.contact.lastName}></input>
+				<input type={'email'} placeholder={'Email'} value={this.props.contact.email}></input>
+				<button type={'submit'}>Add contact</button>
 			</form>
 			)
 	}
