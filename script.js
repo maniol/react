18a313ebc7ae7000ab33c06bd34eeae112c9c911
magda <magdalena.aniol@gmail.com>
2018-07-06 11:39:16 +0200

    -add counters

--- script.js
+++ script.js
@@ -1,2 +1,54 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+var CounterAddition = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+	 increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+	 decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
+	render: function() {
+		return React.createElement('div', {},
+			React.createElement('h1', {}, this.state.counter),
+			React.createElement('button', {onClick: this.increment}, '+'),
+			React.createElement('button', {onClick: this.decrement}, '-'),
+			);
+		}
+});
+
+var CounterSquare = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+	 multiply: function() {
+		this.setState({
+			counter: this.state.counter ** 2
+		});
+	},
+
+	render: function() {
+		return React.createElement('div', {},
+			React.createElement('h1', {}, this.state.counter),
+			React.createElement('button', {onClick: this.increment}, '**'),
+			);
+		}
+});
+
+var elementAddition = React.createElement(CounterAddition);
+var elementSquare = React.createElement(CounterSquare);
+
+var counters = [];
+counters.push(elementAddition, elementSquare)
+ReactDOM.render(elementAddition, document.getElementById('app'));
+
+ReactDOM.render(counters, document.getElementById('app'));
