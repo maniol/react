d4690b386d75b7a3a4ca903a019d08b5472b6c8c
magda <magdalena.aniol@gmail.com>
2018-07-06 13:57:08 +0200

    transform to JSX

--- components/Contact.js
+++ components/Contact.js
@@ -2,18 +2,16 @@ var Contact = React.createClass({
 	propTypes: {
 		item: React.PropTypes.object.isRequired,
 	},
-
 	render: function() {
 		return (
-			React.createElement('div', {className: 'contactItem'},
-				React.createElement('img', {className: 'contactImage',src: 'images\\icon.png'}),
-				React.createElement('div', {className: 'contactInfo'},
-					React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
-					React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
-					React.createElement('a', {className: 'contactEmail', href: this.props.item.email},
-					this.props.item.email)
-				)
-			)
+			<div className={'contactItem'}>
+				<img className={'contactImage'} src={'images\\icon.png'}/>
+				<div className={'contactInfo'}>
+					<p className={'contactLabel'}>{this.props.item.firstName}</p>
+					<p className={'contactLabel'}>{this.props.item.lastName}</p>
+					<a className={'contactEmail'} href={this.props.item.email}>{this.props.item.email}</a>
+				</div>
+			</div>
 		)
 	},
 });
