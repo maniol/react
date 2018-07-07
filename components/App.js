0d38254c25ec547bd42acaba75c2b01034022e6f
magda <magdalena.aniol@gmail.com>
2018-07-07 12:07:39 +0200

    fix jsx syntax

--- components/App.js
+++ components/App.js
@@ -29,15 +29,9 @@ var App = React.createClass({
 	render: function() {
 		return (
 			<div className={app}>
-				<form className={contactForm}>
-					<input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}></input>
-					<input type={'text'} placeholder={'Last Name'} value={this.props.contact.lastName}></input>
-					<input type={'email'} placeholder={'Email'} value={this.props.contact.email}></input>
-					<button type={'submit'}>Add contact</button>
-				</form>
-				/* tu musze dodac funkcje z petla ktora generuje html dla kazdego kontaktu - o to chodzi?*/
-				React.createElement(Contacts, {items: contacts}, {})
+				<ContactForm contact={contactForm}/>
+				<Contacts items={contacts}/>
+			</div>
 			)
-		);
-	}
+		}
 });
